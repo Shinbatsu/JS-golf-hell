6b958ebd17fc320ea6b65d222e4a0e26594db6f7
Shinbatsu <nojic132@gmail.com>
2022-02-11 19:55:12 +0300

    added one interview question

---
+++ Сarry function.js
@@ -0,0 +1,8 @@
+//Your task is write the function which accumulate 
+//sum of digits from given arguments with carry-like 
+//syntax form while not will given empty call
+// s(10)(10)(20)(50)() 90
+// s(10.2)(0)(1)() 11.2
+//a = s(1)(2)(3); // function
+//a(); // 6
+s=a=r=>f=d=>d!==s.b&&(r+=d)&&f||r
