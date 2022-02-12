50081db99775c919109f4e974b5c76f29b1a17d7
Shinbatsu <nojic132@gmail.com>
2022-02-13 00:09:03 +0300

    added one more solution for move zeros task

--- Move Zeros.js
+++ Move Zeros.js
@@ -4,3 +4,5 @@
 //Example:
 //F([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 F=a=>[...a.filter($=>$!==0),...a.filter($=>$===0)]
+// One more solution
+F=a=>a.reduce((x,e)=>(e===0?x.push(e):x.unshift(e),x),[])
